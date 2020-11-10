b0a599519437d9bda32ddb5413c916eda3c9abec
chrislargent <christine.com>
2020-11-09 19:09:38 -0500

    Add project files.

---
+++ Data/EmployeeContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeesCh12.Models;
+
+namespace EmployeesCh12.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Benefits> Benefits { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmentLocation> DepartmentLocations { get; set; }
+        public DbSet<Employee>Employees { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //composite key
+            modelBuilder.Entity<DepartmentLocation>().HasKey(d => new { d.DepartmentID, d.LocationID });
+            //one to many between DepartmentID and DepartmentLocation
+            modelBuilder.Entity<DepartmentLocation>().HasOne(dl => dl.Department)
+                .WithMany(d => d.DepartmentLocations).HasForeignKey(dl => dl.DepartmentID);
+            //one to many between Location and DepartmentLocation
+            modelBuilder.Entity<DepartmentLocation>().HasOne(dl => dl.Location)
+                .WithMany(d => d.DepartmentLocations).HasForeignKey(dl => dl.LocationID);
+
+            //One to many relationship defined both ways with foreign keys 
+            modelBuilder.Entity<Benefits>().HasOne<Employee>(p => p.Employee)
+                .WithOne(s => s.Benefits).HasForeignKey<Employee>(b => b.BenefitID);
+            modelBuilder.Entity<Employee>().HasOne<Benefits>(p => p.Benefits)
+                .WithOne(s => s.Employee).HasForeignKey<Benefits>(e => e.EmployeeID);
+
+        }
+
+    }
+}
---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        //Composite primary key and foreign key for Dept
+        public int DepartmentID { get; set; }
+
+        //Composite primary key and foreign key for Location 
+        public int LocationID { get; set; }
+
+        //navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+        
+
+
+    }
+}
